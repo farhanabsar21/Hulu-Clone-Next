fc2b6fbf00c3e9c49ea3d6d2ef457ca0301a2ea0
farhan21 <65369895+farhanabsar21@users.noreply.github.com>
2021-06-20 20:27:24 +0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,21 @@
- module.exports = {
-
-   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-      extend: {},
+module.exports = {
+  mode: "jit",
+  purge: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      screen: {
+        "3xl": "2000px",
+      }
     },
-    variants: {
-      extend: {},
-    },
-    plugins: [],
-  }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('tailwind-scrollbar-hide')
+  ]
+}
